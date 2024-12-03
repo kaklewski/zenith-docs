03a0d298353ac426c5b83948ac5ec722e75b10ec
Oskar <oskarkaklewski@gmail.com>
2024-12-03 20:24:55 +0100

    feat: Add loading initial content from the DB

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,37 +1,57 @@
 import { useEffect, useRef, useState } from 'react'
 import ReactQuill from 'react-quill'
-import { setDoc, doc } from 'firebase/firestore'
+import { setDoc, doc, getDoc } from 'firebase/firestore'
 import { db } from '../firebase-config'
+import 'react-quill/dist/quill.snow.css'
 
 export default function TextEditor() {
   const quillRef = useRef<any>(null)
   const [isEditing, setIsEditing] = useState<boolean>(false)
 
+  const isLocalChange = useRef<boolean>(false)
+
   const documentRef = doc(db, 'documents', 'sample-doc')
   const saveContent = () => {
-    if (quillRef.current) {
+    if (quillRef.current && isLocalChange.current) {
       const content = quillRef.current.getEditor().getContents()
       console.log(`Saving content to db: ${content}`)
 
       setDoc(documentRef, { content: content.ops }, { merge: true })
         .then(() => console.log('Content saved'))
         .catch(console.error)
+
+      isLocalChange.current = false
     }
   }
 
   useEffect(() => {
     if (quillRef.current) {
       // Load initial content from the Firestore DB
+      getDoc(documentRef)
+        .then(docSnap => {
+          if (docSnap.exists()) {
+            const savedContent = docSnap.data().content
+            if (savedContent) {
+              quillRef.current.getEditor().setContents(savedContent)
+            }
+          } else {
+            console.log(`No document found. Starting with an empty editor.`)
+          }
+        })
+        .catch(console.error)
 
       // Listen to Firestore for any updates and update locally in real time
 
       // Listen for Local text changes and save it to Firestore
       const editor = quillRef.current.getEditor()
-      editor.on('text-change', () => {
-        setIsEditing(true)
-        saveContent()
+      editor.on('text-change', (delta: any, oldDelta: any, source: any) => {
+        if (source === 'user') {
+          isLocalChange.current = true
+          setIsEditing(true)
+          saveContent()
 
-        setTimeout(() => setIsEditing(false), 5000)
+          setTimeout(() => setIsEditing(false), 5000)
+        }
       })
     }
   }, [])
